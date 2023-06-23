2060427477daab137dd203c249a69e265d8f8ec2
Iván Lizzt <6182214+Alelizzt@users.noreply.github.com>
2023-06-23 17:25:04 -0500

    Dealing with errors in Fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,35 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import Category from './components/Category';
 
-import { fetcher } from './fetcher';
+import { getCategories, getProducts } from './fetcher';
 
 function App() {
-  const [categories, setCategories] = useState([]);
-  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState({ errorMessage: '', data: [] });
+  const [products, setProducts] = useState({ errorMessage: '', data: [] });
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetcher("/categories");
-      setCategories(data);
+      const responseObject = await getCategories();
+      setCategories(responseObject);
     }
     fetchData();
   }, [])
 
   const handleCategoryClick = id => {
-    fetch("http://localhost:3000/products?catId=" + id)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setProducts(data);
-      })
+    const fetchData = async () => {
+      const responseObject = await getProducts(id);
+      setProducts(responseObject);
+    }
+    fetchData();
   }
 
   const renderCategories = () => {
-    return categories.map(cat =>
+    return categories.data.map(cat =>
       <Category key={cat.id} id={cat.id} title={cat.title} onCategoryClick={() => handleCategoryClick(cat.id)} />)
   }
 
   const renderProducts = () => {
-    return products.map(p =>
+    return products.data.map(p =>
       <div key={p.id}>{p.title}</div>
     )
   }
@@ -42,12 +41,14 @@ function App() {
 
       <section>
         <nav>
+          {categories.errorMessage && <div> Error: {categories.errorMessage}</div>}
           {
-            categories && renderCategories()
+            categories.data && renderCategories()
           }
         </nav>
         <article>
           <h1>Products</h1>
+          {products.errorMessage && <div> Error: {products.errorMessage}</div>}
           {
             products && renderProducts()
           }
