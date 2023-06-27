1211eb2cc47c9e78b93fef791b784fd9447d3c9c
Iván Lizzt <6182214+Alelizzt@users.noreply.github.com>
2023-06-27 17:15:13 -0500

    Routing on Detail page

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+
 import {
   BrowserRouter,
   Routes,
   Route
 } from 'react-router-dom';
 
+import ProductDetail from './components/ProductDetail';
+import Basket from './components/Basket.jsx';
+import Checkout from './components/Checkout.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
+        <Route path='basket' element={<Basket />} />
+        <Route path='checkout' element={<Checkout />} />
+        <Route path='products/:productId' element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
