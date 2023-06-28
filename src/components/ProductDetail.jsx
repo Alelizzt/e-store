3db2da01f9fd149d1dcaa748a37ed3e6d310e676
Iván Lizzt <6182214+Alelizzt@users.noreply.github.com>
2023-06-28 14:20:16 -0500

    Fetch API - get product by id

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,7 +1,22 @@
+import { useParams } from "react-router-dom"
+
+import { getProductsById } from "../fetcher";
+import { useEffect, useState } from "react";
 
 const ProductDetail = () => {
+    const [product, setProduct] = useState({ errorMessage: '', data: [] });
+    const { productId } = useParams();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const responseObject = await getProductsById(productId);
+            setProduct(responseObject);
+        }
+        fetchData();
+    }, [productId])
+
     return (
-        <div>ProductDetail</div>
+        <div>ProductDetail id:{productId} title:{product.data.title}</div>
     )
 }
 
--- src/fetcher.js
+++ src/fetcher.js
@@ -25,3 +25,7 @@ export const getCategories = () => {
 export const getProducts = (id) => {
     return fetcher("/products?catId=" + id);
 };
+
+export const getProductsById = (id) => {
+    return fetcher("/products/" + id);
+};
