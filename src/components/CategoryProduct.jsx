e101554d9b522f9840af90c83098ff3d1baada08
Iván Lizzt <6182214+Alelizzt@users.noreply.github.com>
2023-06-30 17:57:08 -0500

    Styled components

--- src/components/CategoryProduct.jsx
+++ src/components/CategoryProduct.jsx
@@ -1,64 +1,73 @@
 import PropTypes from 'prop-types';
 import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
-
-const CategoryProduct = ({ id, title, image, specs, features, price, stock }) => {
-
+const CategoryProduct = ({
+    id,
+    title,
+    image,
+    specs,
+    features,
+    price,
+    stock,
+}) => {
     const navigate = useNavigate();
 
     return (
-        <article>
-            <div className='category-product-title'>
-                <Link to={`products/${id}`}> {title} </Link>
-            </div>
+        <ProductInfoArticle>
+            <ProductTitle>
+                <Link to={`/products/${id}`}>{title}</Link>
+            </ProductTitle>
 
             <figure>
-                <div className='category-product-image-container'>
-                    <img src={`./assets/${image}`} alt={title} />
-                </div>
+                <ProductImageContainer>
+                    <ProductImage src={`/assets/${image}`} alt={title} />
+                </ProductImageContainer>
             </figure>
 
             <aside>
-                <div className='category-product-info-dimensions'>
-                    <h3> Dimensions </h3>
+                <ProductInfo>
+                    <ProductInfoHeader>Dimensions</ProductInfoHeader>
                     <label>{specs.dimensions}</label>
-                </div>
+                </ProductInfo>
 
-                {specs.capacity &&
-                    <div className='category-product-info-capacity'>
-                        <h3> Capacity </h3>
+                {specs.capacity && (
+                    <ProductInfo>
+                        <ProductInfoHeader>Capacity</ProductInfoHeader>
                         <label>{specs.capacity}</label>
-                    </div>
-                }
+                    </ProductInfo>
+                )}
 
-                <div className='category-product-info-features'>
-                    <h3> Features </h3>
+                <ProductInfo>
+                    <ProductInfoHeader>Features</ProductInfoHeader>
                     <ul>
-                        {features?.map((feature, i) => {
-                            return <li key={i}>{feature}</li>
+                        {features?.map((f, i) => {
+                            return <ProductInfoListItem key={`feature${i}`}>{f}</ProductInfoListItem>;
                         })}
                     </ul>
-                </div>
+                </ProductInfo>
             </aside>
 
-            <aside className='category-product-info-finance'>
-                <div className='category-product-info-finance-price'>
+            <aside>
+                <ProductInfoFinancePrice>
                     &pound;{price}
-                </div>
+                </ProductInfoFinancePrice>
 
-                <div className='category-product-finance-info-stock'>
-                    <label> Stock level : {stock} </label>
-                    <label>FREE Delivery</label>
-                </div>
+                <ProductInfoStock>
+                    <ProductInfoStockLabel>Stock Level: {stock}</ProductInfoStockLabel>
+                    <ProductInfoStockLabel>FREE Delivery</ProductInfoStockLabel>
+                </ProductInfoStock>
 
-                <div className='category-product-action'>
-                    <button onClick={() => navigate(`products/${id}`)}>View Product</button>
-                    <button>Add to Basket</button>
-                </div>
+                <ProductInfoAction>
+                    <ProductInfoActionButton onClick={() => navigate(`/products/${id}`)}>
+                        View Product
+                    </ProductInfoActionButton>
+                    <ProductInfoActionButton>Add to Basket</ProductInfoActionButton>
+                </ProductInfoAction>
             </aside>
-        </article>
-    )
-}
+        </ProductInfoArticle>
+    );
+};
 
 CategoryProduct.propTypes = {
     id: PropTypes.number,
@@ -70,4 +79,86 @@ CategoryProduct.propTypes = {
     stock: PropTypes.number
 }
 
-export default CategoryProduct
+export default CategoryProduct;
+
+const ProductInfoArticle = styled.article`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 0.25fr 1fr 0.25fr;
+    column-gap: 20px;
+`;
+
+const ProductTitle = styled.div`
+        grid-column: 1 / span 3;
+        color: darkslategray;
+        font-weight: bold;
+        font-size: 1.5em;
+        padding-left: 10px;
+    `;
+
+const ProductImageContainer = styled.div`
+    padding: 10px;
+    width: 60%;
+`;
+
+const ProductImage = styled.img`
+    width: 100%;
+    height: 100%;
+`;
+
+const ProductInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const ProductInfoHeader = styled.h3`
+    color: darkslategray;
+    font-size: 1em;
+    font-weight: bold;
+    padding-top: 10px;
+    padding-bottom: 5px;
+`;
+
+const ProductInfoListItem = styled.li`
+    padding-top: 5px;
+`;
+
+const ProductInfoStock = styled.div`
+    padding-left: 10px;
+    margin-top: 20px;
+    padding-top: 10px;
+    background-color: lightgrey;
+    height: 20%;
+    width: 30%;
+    border-radius: 5px;
+    font-weight: bold;
+    display: flex;
+    flex-direction: column;
+`;
+
+const ProductInfoStockLabel = styled.label`
+    padding-bottom: 5px;
+`;
+
+const ProductInfoAction = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const ProductInfoActionButton = styled.button`
+    width: 160px;
+    height: 30px;
+    border-radius: 10px;
+    margin-top: 20px;
+    background-color: lightgray;
+    border: solid 1px slategrey;
+    font-weight: bold;
+`;
+
+const ProductInfoFinancePrice = styled.div`
+    color: darkslategray;
+    font-size: 2em;
+    font-weight: bold;
+    padding-top: 10px;
+`;
+
